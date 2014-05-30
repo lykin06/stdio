cff3d766c1f54821af7d497bc3c41cfccb386a0a
lykin06 <lykin06@gmail.com>
2014-05-30 19:59:02 +0200

    _flsbuf

--- stdio.c
+++ stdio.c
@@ -63,7 +63,7 @@ int _filbuf(FILE * f) {
 		f->_ptr = f->_base;
 	}
 
-	f->_cnt = read(f->_file, f->_ptr, f->_bufsiz);
+	f->_cnt = read(fileno(f), f->_ptr, f->_bufsiz);
 
 	if(f->_cnt <= 0) {
 		// Soit on a une erreure de lecture, soit on est en fin de fichier
@@ -77,4 +77,83 @@ int _filbuf(FILE * f) {
 	return *f->_ptr;
 }
 
+/*
+ * Ecriture du caractere courant du buffer dans le fichier.
+ * Retourne le caractere en hexa en cas de succes
+ * Retourne -1 en cas d'echec
+ */
+ int _flsbuf(unsigned char c, FILE *f) {
+ 	// Compteur de cractere dans le buffer
+ 	int count;
+
+ 	// Compteur de caractere ecrit
+ 	int written;
+
+ 	// Le fichier
+ 	char file = fileno(f);
+
+ 	// On verefie que le fichier soit ouvert en ecriture
+ 	if (!(f->_flag & _IOWRT)) {
+ 		// On indique qu'il y a une erreur
+ 		f->_flag |= _IOERR;
+ 		return -1;
+ 	}
+
+ 	// Si le fichier est en lecture, on verifie qu'on soit bien au bout
+ 	if (f->_flag & _IOREAD) {
+ 		f->_cnt = 0;
+ 		if (f->_flag & _IOEOF) {
+ 			// On se replace sur la base
+ 			f->_ptr = f->_base;
+ 			f->_flag &= ~_IOREAD;
+ 		} else {
+ 			// On indique une erreur
+ 			f->_flag |= _IOERR;
+ 			return -1;
+ 		}
+ 	}
+
+ 	// On indique qu'on est en ecriture et en debut de fichier
+ 	f->_flag |= _IOWRT;
+ 	f->_flag &= ~_IOEOF;
+ 	written = count = f->_cnt = 0;
+
+ 	// On alloue un buffer s'il y en a pas
+ 	if (f->_flag & _IONBF) {
+ 		alloc_buf(f);
+ 	}
+
+ 	// On verifie que l'on soit en bufferisation totale
+ 	if (f->_flag & _IOFBF) {
+ 		// On voit s'il y a des caracteres dans le buffer
+ 		count = f->_ptr - f->_base;
+
+ 		// On avance le caractere courant
+ 		f->_ptr = f->_base + 1;
+
+ 		// On diminue le nombre de caractere dans le buffer de 1
+ 		f->_cnt = f->_bufsiz - 1;
+
+ 		// S'il reste des caracteres dans le buffer, on les ecrits dans le fichier
+ 		if (count > 0) {
+ 			written = write(file, f->_base, count);
+ 		}
+
+ 		// On ajoute le caractere a la base
+ 		*f->_base = c;
+ 	} else {
+ 		// Sinon on ecrit directement dans le fichier
+ 		count = 1;
+ 		written = write(file, &c, count);
+ 	}
+
+ 	// On verifie que l'ecriture s'est bien passee
+ 	if (written != count) {
+ 		f->_flag |= _IOERR;
+ 		return -1;
+ 	}
+
+ 	// On retourne le caractere ecrit en hexadecimale
+ 	return c & 0xff;
+ }
 
